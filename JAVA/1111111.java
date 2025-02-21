9903c2a300a05af21ee9aeca093fc2ae3e11c02a
YoonHan0 <dbsgksdud12@naver.com>
2025-02-21 11:02:49 +0900

    문자열 나누기 LV1

---
+++ JAVA/1111111.java
@@ -0,0 +1,28 @@
+package JAVA;
+
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+		int result = soluation("1234567", "7");
+		System.out.println(result);
+
+    }
+
+    public static int soluation(String t, String p) {
+		int answer = 0;
+		int start = 0;
+		int chunkSize = p.length();
+		int numberP = Integer.parseInt(p);
+
+		while( start < t.length() ) {
+			int end = Math.min( start+chunkSize, t.length() );
+			int numberT = Integer.parseInt( t.substring(start, end) );
+
+			if( numberP >= numberT ) { answer++; }
+			start = start + chunkSize;
+		}
+		return answer;
+	}
+}
