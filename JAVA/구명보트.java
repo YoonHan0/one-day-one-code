0114c8635dd7860a059c74caa15e25a05d341b77
YoonHan0 <dbsgksdud12@naver.com>
2025-03-28 00:16:06 +0900

    구명보트 틀림..

--- JAVA/구명보트.java
+++ JAVA/구명보트.java
@@ -2,21 +2,37 @@ public class 구명보트 {
     
     public static void main(String[] args) {
 
-		System.out.println("Hello World! " + solution(new int[]{70, 50, 80, 50}, 100));	// 3
+		System.out.println("Hello World! " + solution(new int[]{100, 50, 80, 50}, 150));	// 2
 		System.out.println("Hello World! " + solution(new int[]{70, 80, 50}, 100));		// 3
+
+	}
+	public static int solution(int[] people, int limit) {
+		List<Integer> stack = new ArrayList<>();
+		int result = 0;
+
+		for(int i=0; i<people.length; i++) {
+			stack.add(people[i]);
+			if(stack.size() >= 2) {	// 최대인원
+				if(limit >= sum(stack)) {
+					stack.clear();
+					result++;
+				}
+				else {
+					int maxValue = Collections.max(stack);
+					int maxIndex = stack.indexOf(maxValue);
+					stack.remove(maxIndex);
+					result++;
+				}
+			}
+		}
+		return result + stack.size();
 	}
 
-	public static int solution(int[] people, int limit) {
-		int temp = people[0];
-		int result = 0;
-		for(int i=1; i<people.length; i++) {
-			/*
-			* temp, people[i]랑 비교
-			* 두 개 더해서 limit보다 크면 temp = people[i], 둘 중에 작은 수를 temp로 초기화, result++
-			* 					 작으면 temp = 0, result++
-			* */
+	public static int sum(List<Integer> stack) {
+		int sum = 0;
+		for (int num : stack) {
+			sum += num;
 		}
-		// for문이 끝나고 temp > 0 이면 result++
-		return result;
+		return sum;
 	}
 }
